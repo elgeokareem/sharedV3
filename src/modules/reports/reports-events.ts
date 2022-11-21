58c28c7da4a768eb7528ab87bd341c64f60d117f
Ricardo Moreno <morenoricardo237@gmail.com>
2022-11-21 15:22:39 -0500

    remove react

--- src/modules/reports/reports-events.ts
+++
@@ -1,8 +0,0 @@
-import { createEvent } from '@cobuildlab/react-simple-state';
-
-export const OnSecuredCasesBySpotterEvent = createEvent<{
-  camerasByBranch: any;
-  camerasByUser: any;
-  cameraHitsAndUsers: any;
-}>();
-export const OnSecuredCasesBySpotterEventError = createEvent();
