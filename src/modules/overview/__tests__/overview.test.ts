2de6a519a5faf88240168124efd51cbbb920268e
Patrick Nguyen <iamtiredofyou2@gmail.com>
2022-12-14 07:57:55 -0500

    Adjusted missed repo logic to include previous year for comparison

--- src/modules/overview/__tests__/overview.test.ts
+++ src/modules/overview/__tests__/overview.test.ts
@@ -3,22 +3,39 @@ import { fetchMissedRepossessions } from '../overview-services';
 import { createClient } from '../../../shared/graphql';
 
 describe('Branch Tests', () => {
-  const endpoint = 'http://localhost:5000/graphql';
+  const endpoint = 'https://api.insightt.io/graphql';
   const token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzE5LCJkYk5hbWUiOiJycmFfZGIiLCJpYXQiOjE2Njk2NTIyNzgsImV4cCI6MTY3MjI0NDI3OH0.ZHeFXjfvHys6EmcD2ebmO86f14Y-DCnYFNGRN5n2lXY';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzU4LCJkYk5hbWUiOiJycmFfZGIiLCJpYXQiOjE2NzEwMTcxNzEsImV4cCI6MTY3MzYwOTE3MX0.pU7z0AuAH5-nPO4yKRfNEYFskfH-8g0kqYYiwjFXLLU';
   const gqlClient = createClient(endpoint, token);
-  const startDate = '2022-12-01';
-  const endDate = '2022-12-31';
+  const startDate = '2022-12-01 07:00:00';
+  const endDate = '2022-12-31 07:00:00';
+  const previousStartDate = '2021-12-01 07:00:00';
+  const previousEndDate = '2021-12-31 07:00:00';
 
   test('it fetches all the branches', async () => {
-    // const missedRepos = await fetchMissedRepossessions(gqlClient, startDate, endDate);
-    // expect(missedRepos.length).toBe(50);
+    const missedRepos = await fetchMissedRepossessions(
+      gqlClient,
+      startDate,
+      endDate,
+      previousStartDate,
+      previousEndDate,
+    );
+
+    expect(missedRepos?.current?.length).toBe(64);
+    expect(missedRepos?.previous?.length).toBe(56);
     expect(true).toBeTruthy();
   });
 
   test('it fetches all the branches, filtered by Insight branch', async () => {
-    // const missedReposWithBranchFiltering = await fetchMissedRepossessions(gqlClient, startDate, endDate, 1);
+    // const missedReposWithBranchFiltering = await fetchMissedRepossessions(
+    //   gqlClient,
+    //   startDate,
+    //   endDate,
+    //   previousStartDate,
+    //   previousEndDate,
+    //   1,
+    // );
     // expect(missedReposWithBranchFiltering.length).toBe(23);
-    expect(true).toBeTruthy();
+    // expect(true).toBeTruthy();
   });
 });
--- src/modules/overview/queries.ts
+++ src/modules/overview/queries.ts
@@ -1,18 +1,41 @@
 import gql from 'graphql-tag';
 
 export const MISSED_REPOSSESSIONS_QUERY = gql`
-query MissedRepossessions($where: RDNCaseWhereInput, $orderBy: [RDNCaseOrderByWithRelationInput!]){
-  rDNCases(where: $where, orderBy: $orderBy) {
-    caseId
-    status
-    orderType
-    originalCloseDate
-    originalHoldDate
-    originalOrderDate
-    spottedDate
-    vendorBranchName
-    missedRepossession {
-      createdAt
+  query MissedRepossessions(
+    $where1: RDNCaseWhereInput
+    $where2: RDNCaseWhereInput
+    $orderBy: [RDNCaseOrderByWithRelationInput!]
+  ) {
+    current: rDNCases(where: $where1, orderBy: $orderBy) {
+      caseId
+      status
+      orderType
+      close_date
+      hold_date
+      originalOrderDate
+      spottedDate
+      vendorBranchName
+      lender_client_id
+
+      missedRepossession {
+        createdAt
+      }
+    }
+
+    previous: rDNCases(where: $where2, orderBy: $orderBy) {
+      caseId
+      status
+      orderType
+      close_date
+      hold_date
+      originalOrderDate
+      spottedDate
+      vendorBranchName
+      lender_client_id
+
+      missedRepossession {
+        createdAt
+      }
     }
   }
-}`;
+`;
