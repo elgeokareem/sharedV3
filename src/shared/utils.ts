79385ac2c6d6efafdb6f94462adddedad7aca928
Leonel Lima <leonellimap@gmail.com>
2023-01-17 09:35:00 -0400

    [feat][main] Add getTimeRangeShift and isShiftActive Functions

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,6 +1,8 @@
 /* eslint-disable jsdoc/no-types */
-import { DriverType, UserType } from './types';
+import moment, { Moment } from 'moment';
+
 import packageJson from '../../package.json';
+import { DriverType, ShiftType, UserType } from './types';
 
 /**
  * Replace multi spaces with single spaces.
@@ -77,5 +79,79 @@ export const filterUser = (user: UserType, search: string) => {
 
   return filterUserByFullName(fullName, search);
 };
+
+/**
+ * Check if a shift is active.
+ * 
+ * @param {ShiftType} shift - The shift.
+ * @param {Moment} startTime - Start time of shift 
+ * @param {Moment} endTime - End time of shift.
+ * @param {Moment} momentNow - Current Time.
+ * @returns {boolean} If the shift is active or not.
+ */
+export const isShiftActive = (shift: ShiftType, startTime: Moment, endTime: Moment, momentNow = moment()) => {
+  if (!shift) {
+    throw new Error("Shift can't be null");
+  }
+  
+  const { days } = shift;
+
+  if (!days) {
+    throw new Error('Shift needs days');
+  }
+
+  let isActive = false;
+  if (momentNow.isBetween(startTime, endTime, undefined, '[]') === true) {
+    // Check if the day (Mon, Tue, etc.) is one of the days of activity of the shift.
+    isActive = days.includes(momentNow.format('ddd'));
+  }
+
+  return isActive
+}
+
+/**
+ * To get time ranges (yesterday's and today's) of shift.
+ * 
+ * @param {ShiftType} shift - The shift.
+ * @param {Moment} momentNow - Current Time.
+ * @returns Time ranges (today and yesterday).
+ */
+export const getTimeRangeShift = (shift: ShiftType, momentNow = moment()) => {
+  if (!shift) {
+    throw new Error("Shift can't be null");
+  }
+
+  const { startTimeV2, endTimeV2, days } = shift;
+
+  if (!startTimeV2 || !endTimeV2 || !days) {
+    throw new Error('Shift needs days, startTime and endTime');
+  }
+
+  const [startHour, startMinute] = startTimeV2.split(':');
+  const [endHour, endMinute] = endTimeV2.split(':');
+  const momentStartTime = momentNow
+    .clone()
+    .hour(parseInt(startHour))
+    .minute(parseInt(startMinute));
+  const momentEndTime = momentNow
+    .clone()
+    .hour(parseInt(endHour))
+    .minute(parseInt(endMinute));
+
+  // Next Day adjustment
+  if (momentEndTime.isBefore(momentStartTime)) {
+    momentEndTime.add(1, 'days');
+  }
+
+  const momentYesterdayStartTime = momentStartTime.clone().subtract(1, 'days');
+  const momentYesterdayEndTime = momentEndTime.clone().subtract(1, 'days');
+
+  return {
+    startTime: momentStartTime,
+    endTime: momentEndTime,
+    yesterdayStartTime: momentYesterdayStartTime,
+    yesterdayEndTime: momentYesterdayEndTime,
+  };
+}
 
 export const version = (): string => packageJson.version;
