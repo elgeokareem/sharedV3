e73bbcaed71500c029d2b1347319d5b96d527f9d
Jose Villalobos <joseavv4@gmail.com>
2023-01-26 14:46:52 -0400

    update user validator

---
+++ src/modules/users/validators.ts
@@ -0,0 +1,32 @@
+import { UserType } from "shared/types";
+
+export const validateEmail = (email: string) => {
+  if (email?.length === 0) {
+    return false;
+  }
+  const mailFormat =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return mailFormat.test(email);
+};
+
+export const updateUserValidator = (user: UserType) => {
+  if (!user?.firstName) {
+    return 'Please input First Name';
+  }
+
+  if (!user?.lastName) {
+    return 'Please input Last Name';
+  }
+
+  if (!user?.avatar) {
+    return 'Please add a Profile Photo to edit your account';
+  }
+
+  if (!user?.email) {
+    return 'Please input email address';
+  }
+
+  if (!validateEmail(user?.email)) {
+    return 'Please input a valid email address';
+  }
+};
