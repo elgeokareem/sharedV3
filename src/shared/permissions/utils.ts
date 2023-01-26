239f51bfe6e71a6ceb705ff3aeed036d33ce34fc
Leonel Lima <leonellimap@gmail.com>
2023-01-26 11:04:54 -0400

    [refactor][main] Fix bugs with linter

--- src/shared/permissions/utils.ts
+++ src/shared/permissions/utils.ts
@@ -3,8 +3,8 @@ import { RBACproject } from './index';
 /**
  * Method to verify if the subscription type has the required permissions.
  * 
- * @param {string} permission - Permission.
- * @param {string} subscriptionType - Subscription type.
- * @returns {[boolean, string]} - Boolean to indicate whether you have permission; string is message.
+ * @param permission - Permission.
+ * @param subscriptionType - Subscription type.
+ * @returns Boolean to indicate whether you have permission; string is message.
  */
 export const checkPermission = (permission: string, subscriptionType: string) => RBACproject.check(subscriptionType, permission);
